db1d986133381a92ad70f730a92ffc2dc86ce955
Alan Rafael R. Batista <alanrrb@gmail.com>
2015-12-05 17:08:29 -0200

    Add feature visitor visits job details

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   root 'home#index'
+  resources :jobs, only: [:show]
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
