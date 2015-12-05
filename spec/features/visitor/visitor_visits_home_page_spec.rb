e2ad6e601c290000db9b4b4ffbba59be3586ff66
Alan Rafael R. Batista <alanrrb@gmail.com>
2015-12-05 16:10:03 -0200

    Add jobs in home page

--- spec/features/visitor/visitor_visits_home_page_spec.rb
+++ spec/features/visitor/visitor_visits_home_page_spec.rb
@@ -5,4 +5,21 @@ feature 'Visitor visits Emprego Ja home page' do
     visit root_path
     expect(page).to have_content('Emprego Já')
   end
+  scenario 'and see jobs' do
+    Job.create(title: 'Vaga de Dev',
+               category: 'Desenvolvedor',
+               company: 'Campus Code',
+               description: 'Dev Junior Rails com ao menos um projeto',
+               location: 'São Paulo')
+    # Job.create(title: 'Vaga de QA',
+    #            category: 'QA',
+    #            company: 'Campus Code',
+    #            description: 'QA Junior com ao menos um projeto',
+    #            location: 'São Paulo')
+    visit root_path
+    expect(page).to have_content('Vaga de Dev')
+    expect(page).to have_content('Campus Code')
+    expect(page).to have_content('São Paulo')
+    # expect(page).to have_content('Vaga de QA')
+  end
 end
