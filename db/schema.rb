f431d7cbb102cec49e634e5054de6dc6865a30be
Alan Rafael R. Batista <alanrrb@gmail.com>
2015-12-05 16:11:21 -0200

    Add feature home page with jobs

--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20151205180211) do
+
+  create_table "jobs", force: :cascade do |t|
+    t.string   "title"
+    t.string   "category"
+    t.string   "company"
+    t.string   "description"
+    t.string   "location"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
 
 end
