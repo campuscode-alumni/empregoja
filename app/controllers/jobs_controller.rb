762439502cd0881936b0082479dc1d3bdb682524
Alan Rafael R. Batista <alanrrb@gmail.com>
2015-12-05 17:40:30 -0200

    Add feature user creates a new job

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -2,4 +2,20 @@ class JobsController < ApplicationController
   def show
     @job = Job.find(params[:id])
   end
+
+  def new
+    @job = Job.new
+  end
+
+  def create
+    @job = Job.create(job_params)
+    redirect_to @job
+  end
+
+  private
+
+  def job_params
+    params.require(:job)
+      .permit(:title, :location, :category, :company, :description)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   root 'home#index'
-  resources :jobs, only: [:show]
+  resources :jobs, only: [:show, :new, :create]
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
