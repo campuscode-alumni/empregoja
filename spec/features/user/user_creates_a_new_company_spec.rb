2a7dac5ca898e660bf95c183f9ed0990bd027868
Alan Rafael R. Batista <alanrrb@gmail.com>
2015-12-09 19:39:59 -0200

    User should create a new company

---
+++ spec/features/user/user_creates_a_new_company_spec.rb
@@ -0,0 +1,26 @@
+require 'rails_helper'
+
+feature 'User creates a new company' do
+
+  scenario 'successfully' do
+    company = Company.new(name:     'Campus Code',
+                          location: 'São Paulo',
+                          mail:     'contato@campuscode.com.br',
+                          phone:    '2369-3476')
+
+    visit new_company_path
+
+    fill_in 'Name',     with: company.name
+    fill_in 'Location', with: company.location
+    fill_in 'Mail',     with: company.mail
+    fill_in 'Phone',    with: company.phone
+
+    click_on 'Criar Empresa'
+
+    expect(page).to have_content company.name
+    expect(page).to have_content company.location
+    expect(page).to have_content company.mail
+    expect(page).to have_content company.phone
+  end
+
+end
