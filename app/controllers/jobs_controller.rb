681bcf9124acf884b2bd4b5efc3b8833a18a18c2
Alan Rafael R. Batista <alanrrb@gmail.com>
2015-12-10 17:31:57 -0200

    Add feature user creates a valid job

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -1,19 +1,28 @@
 class JobsController < ApplicationController
+  before_action :set_companies, only: [:new, :create]
+
   def show
     @job = Job.find(params[:id])
   end
 
   def new
     @job = Job.new
-    @companies = Company.all
   end
 
   def create
-    @job = Job.create(job_params)
-    redirect_to @job
+    @job = Job.new(job_params)
+    if @job.save
+      redirect_to @job
+    else
+      render :new
+    end
   end
 
   private
+
+  def set_companies
+    @companies = Company.all
+  end
 
   def job_params
     params.require(:job)
--- app/models/job.rb
+++ app/models/job.rb
@@ -1,3 +1,5 @@
 class Job < ActiveRecord::Base
   belongs_to :company
+
+  validates :title, :category, :description, :location, presence: true
 end
