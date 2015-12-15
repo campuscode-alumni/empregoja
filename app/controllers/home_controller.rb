a1bbb092f9ace92690cc33c89af522b54e771b69
Alan Rafael R. Batista <alanrrb@gmail.com>
2015-12-15 19:13:00 -0200

    Add feature user should choose jobs by category

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,5 +2,6 @@ class HomeController < ApplicationController
   def index
     @jobs = Job.all
     @companies = Company.all
+    @categories = Category.all
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   root 'home#index'
   resources :jobs, only: [:show, :new, :create]
   resources :companies, only: [:new, :create, :show]
+  resources :categories, only: [:show]
 end
