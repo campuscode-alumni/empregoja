0b1b132f6670621187bad89b9300f739799b0717
Alan Rafael R. Batista <alanrrb@gmail.com>
2015-12-15 20:55:20 -0200

    User should create a new category

---
+++ spec/features/user/user_creates_new_category_spec.rb
@@ -0,0 +1,22 @@
+require 'rails_helper'
+
+feature 'User creates a new category' do
+
+  scenario 'successfully' do
+    category = Category.new(name: 'Desenvolvedor')
+
+    visit new_category_path
+
+    fill_in 'Name',     with: category.name
+    click_on 'Criar Categoria'
+
+    expect(page).to have_content category.name
+  end
+
+  scenario 'invalid' do
+    visit new_category_path
+    click_on 'Criar Categoria'
+
+    expect(page).to have_content "Warning! All fields are mandatory."
+  end
+end
