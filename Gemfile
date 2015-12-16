d53b9aee567f35f82a303f44d8c27a78a8d0ebeb
Alan Rafael R. Batista <alanrrb@gmail.com>
2015-12-16 17:48:33 -0200

    Add feature visitor see recent jobs

--- Gemfile
+++ Gemfile
@@ -11,6 +11,8 @@ gem 'jbuilder', '~> 2.0'
 gem 'sdoc', '~> 0.4.0', group: :doc
 
 group :development, :test do
+  gem 'pry-rails'
+  gem 'pry-byebug'
   gem 'rspec-rails'
   gem 'simplecov', require: false
   gem 'capybara'
--- app/models/job.rb
+++ app/models/job.rb
@@ -1,6 +1,11 @@
 class Job < ActiveRecord::Base
+  DAYS_TO_BE_RECENT = 5.days
   belongs_to :company
   belongs_to :category
 
   validates :title, :category, :description, :location, presence: true
+
+  def recent?
+    Time.zone.now < (created_at + DAYS_TO_BE_RECENT)
+  end
 end
