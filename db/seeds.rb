6108f173d7fcc3b5bb788c278ddd63c2e0706196
Lucas Machado <lucas.dpmachado@gmail.com>
2016-02-01 21:05:09 -0200

    Fix rspec

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+company = Company.create(name: 'Campus Code', location: 'São Paulo', mail: 'contato@campuscode.com.br', phone: '2369-3476')
+new_company = Company.create(name:     'Code Campus',
+                             location: 'Rio de Janeiro',
+                             mail:     'contato@codecampus.com.br',
+                             phone:    '1111-5555')
+category = Category.create(name: 'Desenvolvedor')
+new_category = Category.create(name: 'Dev Ninja')
+new_job = Job.create(title: 'Dev Master', location: 'Rio de Janeiro', company: new_company, category: new_category, description: 'Vaga para Dev Master para Bootcamp Rails', featured: true)
+job = Job.create(title: 'Vaga de Dev',
+           description: 'Dev Junior Rails com ao menos um projeto',
+           location: 'São Paulo',
+           company: company,
+           category: category)
